8c73bd96eeda98cca44d82fc301923f58c349baa
Kegan Edwards <keganedwards@proton.me>
2024-09-01 09:59:38 -0400

    Update cart.js

--- microservice-frontend/cart.js
+++ microservice-frontend/cart.js
@@ -4,108 +4,108 @@ let itemCount;
 let totalPrice;
 var itemHolder = [];
 
-let email = sessionStorage.getItem('email'); //gets the users email from sessionStorage
- 
+let email = sessionStorage.getItem('email'); // Retrieve the user's email from sessionStorage
+
+// Initialize the cart display
 getCart(email);
- 
-function getCart($email) {
+
+function getCart(email) {
     $.ajax({
         url: Url + 'GetCart',
         type: 'get',
         dataType: 'json',
-        data: {"email":$email},
+        data: { "email": email },
         contentType: 'text/plain',
         success: function (data) {
- 
             list = '';
             listAdd = '';
             itemCount = 0;
             totalPrice = 0;
 
+            // Generate HTML for each cart item
             $.each(data['data']['List'], function (i, item) {
-                listAdd = '<div class="row main align-items-center">\n' +
-                    '                        <div class="col-2"><img class="img-fluid" src="' +
-                  item['image'] + '"></div>\n' +
-                    '                        <div class="col">\n' +
-                    '                            <div class="row text-muted">' + item['operating_system'] + '</div>\n' +
-                    '                            <div class="row">' + item['title'] + '</div>\n' +
-                    '                        </div>\n' +
-                    '                        <div class="col"> <a class="border">1</a></div>\n' + //here the 1
-
-                    '                        <div class="col">&dollar; ' + item['money_price'] + ' <a onclick="deleteItem(' + item['id'] + ')" type="button">&#10005;</a></div>\n' + 
-                    '                    </div>';
-                list = list + listAdd;
+                listAdd = `
+                    <div class="row main align-items-center">
+                        <div class="col-2"><img class="img-fluid" src="${item['image']}"></div>
+                        <div class="col">
+                            <div class="row text-muted">${item['operating_system']}</div>
+                            <div class="row">${item['title']}</div>
+                        </div>
+                        <div class="col"><a class="border">1</a></div>
+                        <div class="col">&dollar; ${item['money_price']} <a onclick="deleteItem(${item['id']})" type="button">&#10005;</a></div>
+                    </div>`;
+                list += listAdd;
                 itemCount++;
-
                 itemHolder.push(item['id']);
-
                 totalPrice += parseInt(item['money_price']);
             });
- 
-            
 
-
+            // Update the cart display
             $('#cart-list').html(list);
-            $('#item-count').html(itemCount + ' items');
-            $('#item-total').html(itemCount + ' items');
-            $('#item-price').html('&dollar; ' + totalPrice);
-       
+            $('#item-count').html(`${itemCount} items`);
+            $('#item-total').html(`${itemCount} items`);
+            $('#item-price').html(`&dollar; ${totalPrice}`);
         },
-        error: function (data) {
+        error: function () {
             alert("Error while fetching data.");
         }
     });
 }
- 
-function deleteItem($id) {
-    
+
+function deleteItem(id) {
     $.ajax({
-        url: Url +'Cart/' + $id,
+        url: Url + 'Cart/' + id,
         type: 'delete',
         dataType: 'json',
         contentType: 'text/plain',
-        success: function (data) {
-            getCart(email);
-      },
-      error: function (data) {
-          alert("Error while fetching data.");
-      }
+        success: function () {
+            getCart(email); // Refresh cart after deletion
+        },
+        error: function () {
+            alert("Error while deleting item.");
+        }
     });
 }
 
-function clearCart() {
-    //this function removes all items from the cart
-    for (let i = 0; i < itemCount; i++)
-    {
-        id = itemHolder.pop();
-        deleteItem(id); 
+function emptyCart() {
+    // Remove all items from the cart
+    while (itemHolder.length > 0) {
+        let id = itemHolder.pop();
+        deleteItem(id);
     }
-} 
-
-function addAnother() {
-    //this adds another of the same item
-    for (let i = 0; i < itemCount; i++)
-    {
-       id = itemHolder.pop();
-       addToCart(id);
-       
-    } 
-    getCart(email)
 }
 
-
+function duplicateItmes() {
+    // Add another of each item to the cart
+    itemHolder.forEach(id => addToCart(id));
+    getCart(email); // Refresh cart after adding items
+}
 
 function checkOut() {
     let email = $('#email').val();
     $.ajax({
-        url: Url +'Cart',
+        url: Url + 'Cart',
         type: 'put',
         dataType: 'json',
-        data: {"email":email}, //the json is defined here using javascript's dictionary syntax.
+        data: { "email": email },
         contentType: 'text/plain',
-        success: function (data) { //on success
-            //reactive HTML that depends on the contents od the returned data
-            comment='';
+        success: function () {
+            // Clear the cart after successful checkout
+            itemHolder = [];
+            $('#cart-list').html('');
+            $('#item-count').html('0 items');
+            $('#item-total').html('0 items');
+            $('#item-price').html('&dollar; 0');
+
+            // Optionally, redirect to a confirmation page
+            // window.location.href = 'confirmation.html';
+
+            // Or show a success message
+            alert('Checkout successful! Thank you for your purchase.');
+        },
+        error: function () {
+            alert("Error during checkout.");
         }
     });
 }
+
